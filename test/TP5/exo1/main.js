8a455df9968f894d8d46ad8fd7d9a83a06c2fb9d
Pompiquoi <camjuman@gmail.com>
2023-10-20 15:33:40 +0200

    exo 2

---
+++ test/TP5/exo1/main.js
@@ -0,0 +1,21 @@
+function onFormSubmit() {
+    // prevent the form to be sent to the server
+    event.preventDefault();
+    let nom = $("#inputNom").val();
+    let prenom = $("#inputPrenom").val();
+    let date = $("#inputDate").val();
+    let aime = $("#inputAime").val();
+    let rmq = $("#inputRmq").val();
+    $("#studentsTableBody").append(`
+            <tr>
+                <td>${nom}</td>
+                <td>${prenom}</td>
+                <td>${date}</td>
+                <td>${aime}</td>
+                <td>${rmq}</td>
+                <td></td>
+                <td></td>
+            </tr>`);
+    this.reset();
+}
+
